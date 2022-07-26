e3653348a317bbea15cbf927a05c33a090969290
sayu06 <109415353+sayu06@users.noreply.github.com>
2022-07-26 20:34:04 +0700

    print numbers from 1 to 10.

---
+++ Print10.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Sayali
+ */
+
+public class Print10 
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+          
+                    
+        }
+    }
+    
+}
+/*Write a program to print numbers from 1 to 10.*/
